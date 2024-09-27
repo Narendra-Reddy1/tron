4dc550188165c6ecb8ea35511554862af4266c9c
narendra_reddy <narendrareddy54321@gmail.com>
2024-09-27 21:58:26 +0530

    fixed path error

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { configRouter } = require("./routes/config");
 const cron = require("node-cron")
 const User = require("./models/User");
 const { getLedgerContract } = require("./core/contracts");
-const { validateToken } = require("./backend/middlewares/checkAuthentication");
+const { validateToken } = require("./middlewares/checkAuthentication");
 const cors = require('cors')
 require("dotenv").config()
 
